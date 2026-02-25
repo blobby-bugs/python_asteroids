c3919ba9dbf2abed7d55bf0e7351bdcafd6088fd
blobby-bugs <bobbodaskank@gmail.com>
2026-02-25 15:53:36 -0700

    add game clock at 60FPS

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +18,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
